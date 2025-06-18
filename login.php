766725a9cfe5ff42ee1506d6fe56a8e2acbfa26e
nakae-t <2301180@s.asojuku.ac.jp>
2025-06-18 12:11:56 +0900

    コミット

--- login.php
+++ login.php
@@ -8,8 +8,8 @@
 <body>
     <form action="Todo_list.php" method="POST">
         <h1>ログイン</h1>
-        <p>ユーザー名：<input type="text" name="user_name"></p>
-        <p>パスワード：<input type="text" name="user_pass"></p>
+        <p>ユーザー名：<input type="text" name="user_name" ></p>
+        <p>パスワード：<input type="text" name="user_pass" ></p>
     <button type="submit">ログイン</button>
     </form>
 
