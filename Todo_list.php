a5b3c58baa6744e4fdf2455b8ad3c1b88adce031
nakae-t <2301180@s.asojuku.ac.jp>
2025-06-18 18:33:29 +0900

    login処理及びファイルの作成

--- Todo_list.php
+++ Todo_list.php
@@ -8,12 +8,14 @@
 <body>
     <h1>ToDoリスト</h1>
     <!-- ログイン中のアカウント名を追加 -->
-    <p> <a href="">ログアウト</a></p>
+    <p> <a href="./login.php">ログアウト</a></p>
 
+    <!-- リストの内容を追加 -->
     <form action="">
-
+    
     </form>
 
+    <!-- リスト内容の変更 -->
     <form action="">
         
     </form>
--- new_user.php
+++ new_user.php
@@ -1,23 +1,29 @@
+<?php
+require_once './config/connectionDB.php';
+if(isset($_POST["Registra"])){
+        $user_name = $_POST["user_name"] ?? "";
+        $user_pass = $_POST["user_pass"] ?? "";
+
+        $stmt = $pdo->prepare("INSERT INTO users(username, password) VALUES (?, ?)");
+        $stmt->execute([$user_name,$user_pass]);
+        header("Location:https://aso2301180.pinoko.jp/ToDo/login.php");
+        exit;
+    }
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Document</title>    
 </head>
 <body>
     <h1>ユーザー登録</h1>
     <form action="./new_user.php" method="POST">
         <p>ユーザー名：<input type="text" name="user_name"></p>
-        <p>パスワード：<input type="text" name="user_pass"></p>
+        <p>パスワード：<input type="password" name="user_pass"></p>
     <button type="submit" name="Registra">登録</button>
     </form> 
     <a href="./login.php">ログインはこちら</a>
-    <?php
-    if(isset($_POST["Registra"])){
-        
-    }
-
-    ?>
 </body>
 </html>
