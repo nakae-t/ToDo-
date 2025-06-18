a677ebc09d7912b5374b3e69688a54e55f42ee7c
Yuta-2301199 <2301199@s.asojuku.ac.jp>
2025-06-18 12:27:18 +0900

    login.phpの修正

--- login.php
+++ login.php
@@ -3,16 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>ログイン</title>
 </head>
 <body>
-    <form action="Todo_list.php" method="POST">
-        <h1>ログイン</h1>
-        <p>ユーザー名：<input type="text" name="user_name" ></p>
-        <p>パスワード：<input type="text" name="user_pass" ></p>
-    <button type="submit">ログイン</button>
+    <h1>ログイン</h1>
+    <form action="loginAuthenticate.php" method="POST">
+        <p>
+            <label for="user_name">ユーザー名：</label>
+            <input type="text" name="user_name" required>
+        </p>
+        <p>
+            <label for="user_pass">パスワード：</label>
+            <input type="text" name="user_pass" required>
+        </p>
+        <button type="submit">ログイン</button>
     </form>
-
-    <a href="new_user.php">新規登録</a>   
+    <p>
+        <a href="new_user.php">新規登録</a>
+    </p>
 </body>
 </html>
